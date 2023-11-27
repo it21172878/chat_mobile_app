d83c179ebbccb06822787cb40715f80ccb64202f
Dilanka Liyanagama <prasadliyanagamad@gmail.com>
2023-11-27 16:56:17 +0530

    Handling signed-in users

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:chat_mobile_app/helper/helper_function.dart';
+import 'package:chat_mobile_app/pages/auth/login_page.dart';
 import 'package:chat_mobile_app/pages/home_page.dart';
 import 'package:chat_mobile_app/shared/constants.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -23,14 +25,36 @@ void main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _isSignedIn = false;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getUserLoggedInStatus();
+  }
+
+  getUserLoggedInStatus() async {
+    await HelperFunctions.getUserLoggedInStatus().then((value) {
+      if (value != null) {
+        _isSignedIn = value;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: _isSignedIn ? const HomePage() : const LoginPage(),
     );
   }
 }

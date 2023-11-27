75673ecc091543e0d55f2d79eabf6f767622b1bb
Dilanka Liyanagama <prasadliyanagamad@gmail.com>
2023-11-27 13:50:07 +0530

    connect firebase

---
+++ lib/shared/constants.dart
@@ -0,0 +1,6 @@
+class Constants {
+  static String appId = "1:335815933516:web:9a5e8c96a0522529743ee9";
+  static String apiKey = "AIzaSyDGGyfXqXbH4yx7QgkijK17Hd0XHuujQj4";
+  static String messagingSenderId = "335815933516";
+  static String projectId = "chat-mobile-app-e7aed";
+}

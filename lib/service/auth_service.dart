545bf53d8c5dc22e8d0d1a599210cb29a1041670
Dilanka Liyanagama <prasadliyanagamad@gmail.com>
2023-11-28 17:40:05 +0530

    Firebase Firestore Structure Showcase part1

---
+++ lib/service/auth_service.dart
@@ -0,0 +1,28 @@
+import 'package:chat_mobile_app/service/database_service.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+
+  // login
+
+  // register
+  Future registerUserWithEmailandPassword(
+      String fullName, String email, String password) async {
+    try {
+      User user = (await firebaseAuth.createUserWithEmailAndPassword(
+              email: email, password: password))
+          .user!;
+
+      if (user != null) {
+        // call our database service to update the user data
+        await DatabaseService(uid: user.uid).updateUserData(fullName, email);
+        return true;
+      }
+    } on FirebaseAuthException catch (e) {
+      return e.message;
+    }
+  }
+
+  // signout
+}
